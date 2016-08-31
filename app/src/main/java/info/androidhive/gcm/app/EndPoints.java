d5858b5023a3aad1888c2d665630164195e184e3
Manulanka <Manulanka>
2016-08-31 20:57:43 +0530

    First commit

--- app/src/main/java/info/androidhive/gcm/app/EndPoints.java
+++ app/src/main/java/info/androidhive/gcm/app/EndPoints.java
@@ -8,7 +8,7 @@ public class EndPoints {
     // localhost url
     // public static final String BASE_URL = "http://192.168.0.101/gcm_chat/v1";
 
-    public static final String BASE_URL = "http://192.168.8.100/gcm_chat/v1";
+    public static final String BASE_URL = "http://192.168.8.105/gcm_chat/v1";
     public static final String LOGIN = BASE_URL + "/user/login";
     public static final String USER = BASE_URL + "/user/_ID_";
     public static final String CHAT_ROOMS = BASE_URL + "/chat_rooms";
